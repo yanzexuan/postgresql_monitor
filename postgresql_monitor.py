ff239a800be78c48e88d4a8212972faafa0b5b9b
Evan Yan <Evan.Yan@blackboard.com>
2016-02-05 18:14:16 +0800

    Added output font style, so it looks better for changes/differences

--- postgresql_monitor.py
+++ postgresql_monitor.py
@@ -17,6 +17,51 @@ import json
 from datetime import datetime
 from time import sleep
 
+# utils class
+class Utils:
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    BLACK = '\033[0;30m'
+    RED = '\033[0;31m'
+    GREEN = '\033[0;32m'
+    BROWN = '\033[0;33m'
+    BLUE = '\033[0;34m'
+    PURPLE = '\033[0;35m'
+    CYAN = '\033[0;36m'
+    GREY = '\033[0;37m'
+
+    DARK_GREY = '\033[1;30m'
+    LIGHT_RED = '\033[1;31m'
+    LIGHT_GREEN = '\033[1;32m'
+    YELLOW = '\033[1;33m'
+    LIGHT_BLUE = '\033[1;34m'
+    LIGHT_PURPLE = '\033[1;35m'
+    LIGHT_CYAN = '\033[1;36m'
+    WHITE = '\033[1;37m'
+
+    RESET = "\033[0m"
+
+    @staticmethod
+    def highlight(text):
+        return Utils.BOLD + text + Utils.RESET
+
+    @staticmethod
+    def red(text):
+        return Utils.RED + text + Utils.RESET
+
+    @staticmethod
+    def green(text):
+        return Utils.GREEN + text + Utils.RESET
+
+    @staticmethod
+    def gray(text):
+        return Utils.GREY + text + Utils.RESET
+
+    @staticmethod
+    def strike(text):
+        return u'\u0336'.join(text) + u'\u0336'
+
 
 # write contents to a file
 class FileWriter:
@@ -52,6 +97,7 @@ class DbSnapshot:
                 fw.writeFile()
 
 
+# find the table from tables by table name
 def findTableByName(tables, tableName):
     for t in tables:
         if t[0] == tableName:
@@ -59,12 +105,43 @@ def findTableByName(tables, tableName):
     return None
 
 
+# check if we should care about this table/view. A table/view can be excluded by settings.json
 def isTableIncluded(tableName):
     return ((OnlyDetectTablesAndViews is None or len(OnlyDetectTablesAndViews) == 0) or (tableName in OnlyDetectTablesAndViews)) \
         and (tableName not in ExcludedTablesAndViews)
 
 
-# compare two snapshots
+# compare two table records, compare fields one by one so we can find which field is changed
+# records are tuples (or list), they should have the same length
+# e.g.
+# (58L, 62L, 10L, Decimal('1'), Decimal('3'), 'N', 'N') -> 
+# (57L, 93L, 15L, Decimal('0'), Decimal('3'), 'N', 'N')
+def compareTableRecords(r1, r2):
+    if (type(r1) == tuple and type(r2) == tuple and len(r1) == len(r2)):
+        string2 = '('
+        i = 0
+        while i < len(r1):
+            # kind of tricky here, because we want to print out with tuple format, 
+            # and also want to insert font style. 
+            # So we compare each element in tuple,a nd convert that one to a tuple 
+            # (that only has one element), just want to keep the output string format
+            strTmp = str((r2[i],))
+            strTmp = strTmp[1 : len(strTmp) - 2] # delete the '(' in front and ',)' at end
+            if r1[i] != r2[i]:
+                strTmp = Utils.green(strTmp) # add font style if different
+            if 0 != i:
+                string2 += ", " # split each item
+            string2 += strTmp
+            i += 1
+        string2 += ')'
+        print Utils.gray(str(r1)) + " -> " 
+        print string2
+    else: # if not tuple or len are different
+        print Utils.gray(str(r1)) + " -> " 
+        print str(r2)
+
+
+# compare two snapshots, which contains all tables and views
 # assume s2 is newer than s1, thus this method can detect db changes
 def compareSnapshots(s1, s2):
     #print "Snapshot1 started time: %s, Snapshot2 started time: %s" % (s1.startTime, s2.startTime)
@@ -78,30 +155,31 @@ def compareSnapshots(s1, s2):
             continue
         t2 = findTableByName(s2.tables + s2.views, tableName)
         if not t2: # means table/view is deleted
-            print "Table deleted: " + tableName
+            print "Table deleted: " + Utils.highlight(tableName)
             continue
         if t1[1] != t2[1]:
-            print "%s has different records count: %d -> %d" % (tableName, t1[1], t2[1])
+            print "%s has different records count: %d -> %d" % (Utils.highlight(tableName), t1[1], t2[1])
         rows1 = t1[2]
         rows2 = t2[2]
+        if rows1 == rows2: # no change, then continue
+            continue
+
         # the compare is not so smart, but let it be
-        if rows1 != rows2:
-            print "%s has different records content: " % (tableName)
-            if type(rows1) != list or type(rows2) != list:
-                print str(rows1) + " -> " + str(rows2)
-            else:
-                i = 0
-                while len(rows1) > i and len(rows2) > i:
-                    if rows1[i] != rows2[i]:
-                        print str(rows1[i]) + " -> " 
-                        print str(rows2[i])
-                    i += 1
-                while len(rows1) > i:
-                    print str(rows1[i]) + " -> None"
-                    i += 1
-                while len(rows2) > i:
-                    print " -> None" + str(rows2[i])
-                    i += 1
+        print "%s has different records content: " % (Utils.highlight(tableName))
+        if type(rows1) != list or type(rows2) != list:
+            print str(rows1) + " -> " + str(rows2)
+        else:
+            i = 0
+            while len(rows1) > i and len(rows2) > i:
+                if rows1[i] != rows2[i]:
+                    compareTableRecords(rows1[i], rows2[i])
+                i += 1
+            while len(rows1) > i:
+                print Utils.red("- " + str(rows1[i])) # deleted items
+                i += 1
+            while len(rows2) > i:
+                print Utils.green("+ " + str(rows2[i])) # newly added items
+                i += 1
             
     # find if any new table/view created
     for t2 in s2.tables + s2.views:
@@ -110,7 +188,7 @@ def compareSnapshots(s1, s2):
             continue
         t1 = findTableByName(s1.tables + s1.views, tableName)
         if not t1:
-            print "Table created: " + tableName
+            print "Table created: " + Utils.highlight(tableName)
 
 
 db_database = None
@@ -128,6 +206,7 @@ def loadSettings():
             # read info from json
             settings = json.loads(f.read())
             global db_database, db_user, db_password, OnlyDetectTablesAndViews, ExcludedTablesAndViews, ExecuteTimeInterval
+            db_database = db_user = db_password = OnlyDetectTablesAndViews = ExcludedTablesAndViews = ExecuteTimeInterval = None
             db_database = settings["database"]
             db_user = settings["user"]
             db_password = settings["password"]
@@ -194,6 +273,8 @@ def do():
             db_snapshorts = tmp
 
     except psycopg2.DatabaseError as e: 
+        print ('DatabaseError: %s' % e)
+    except Exception as e: 
         print ('Error: %s' % e)
     finally:
         if db_con:
